affdd514589ebf41cc0a5a4cd103d15eda0e8b3d
Chyngyzkan Samatov <csamatov96@gmail.com>
2025-10-22 00:07:06 -0500

    Upgrade instance and add monitoring

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
 resource "aws_instance" "web" {
   ami           = "ami-12345"
-  instance_type = "t2.micro"
-
+  instance_type = "t2.large"       # Changed instance type
+  monitoring    = true              # Added monitoring
   tags = {
-    Name = "WebServer"
+    Name        = "WebServer"
+    Environment = "Production"      # Different value for Environment
+    Compliance  = "PCI-DSS"
+    Team        = "DevOps"
   }
+}
+
+resource "aws_cloudwatch_alarm" "cpu" {   # Different resource
+  alarm_name = "high-cpu-alarm"
+  metric_name = "CPUUtilization"
 }
