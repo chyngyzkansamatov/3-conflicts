307c38f9352133a22d521c2fdc74dc4e513ee619
Chyngyzkan Samatov <csamatov96@gmail.com>
2025-10-21 23:48:13 -0500

    change env to owner tag

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "web" {
   ami           = "ami-12345"
-  instance_type = "t2.micro"
+  instance_type = "t2.large" # you are changing this. This is NOT a conflict! It's a clean change.  
 
   tags = {
     Name = "WebServer"
+    Owner = "DevOps Team" # you are changing this  
   }
 }
